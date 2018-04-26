b318fd98aaf61ce30550b4dc83c335230027118d
JangMinYoung <min8540055@hanmail.net>
2018-04-26 23:52:12 +0900

    연습문제 phase1_8

---
+++ src/phase1/phase1_8.java
@@ -0,0 +1,14 @@
+package phase1;
+
+import java.util.Scanner;
+
+public class phase1_8 {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		
+		while(scan.hasNext()) {
+			String str = scan.nextLine();
+			System.out.println(str);	
+		}
+	}
+}
